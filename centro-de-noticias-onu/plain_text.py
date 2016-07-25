300883a57ba317eb7fc9f5cc1d0e848639013f7c
fgurtler <fgurtler@gmail.com>
2016-07-25 22:54:31 +0700

    Added converter for cleaned html -> plain text (article body only)

---
+++ centro-de-noticias-onu/plain_text.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import argparse
+from lxml import html
+import glob, os
+    
+def get_plain_text(file_name):
+    with open(file_name, encoding="utf-8") as original_html:
+		tree = html.fromstring(original_html.read())
+		return tree.get_element_by_id('story').text
+
+parser = argparse.ArgumentParser(description='Reduce news articles from UN News to plain text')
+parser.add_argument('source_dir', type=str,  help='source directory containing cleaned .html')
+parser.add_argument('dest_dir', type=str, help='destination directory')
+args = parser.parse_args()
+
+os.chdir(args.source_dir)
+for file in glob.glob("*.html"):
+    print(file)	
+	
+    base = os.path.basename(file)
+    name = os.path.splitext(base)[0]
+
+    with open('../' + args.dest_dir + "//" + name + ".txt", 'w', encoding='utf-8') as plain_text
+		plain_text.write(get_plain_text(file))
