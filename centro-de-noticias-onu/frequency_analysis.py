7d1fb34eb1f00706f52ddf76775b9f920cad5600
fgurtler <fgurtler@gmail.com>
2016-08-04 16:46:05 +0700

    Frequency analysis using TreeTagger for lemma resolution

--- centro-de-noticias-onu/frequency_analysis.py
+++ centro-de-noticias-onu/frequency_analysis.py
@@ -3,20 +3,34 @@ import re
 from nltk.corpus.reader.plaintext import PlaintextCorpusReader
 from nltk.probability import FreqDist
 from nltk.corpus import stopwords
+import treetaggerwrapper
 
-article_corpus = PlaintextCorpusReader('plain_text/', '.*\.txt')
+article_corpus = PlaintextCorpusReader('text_plain/', '.*\.txt', 
+	sent_tokenizer=nltk.data.LazyLoader('tokenizers/punkt/spanish.pickle'))
+
 stop_words = nltk.corpus.stopwords.words('spanish') 
 non_alphabetic = re.compile("\W|\d")
 words = []
+tags = []
 
-#TODO: profile this and see which part is taking 40+ seconds
-for word in article_corpus.words():
-	word = word.lower()
-	if word not in stop_words and not non_alphabetic.search(word):
-		words.append(word)
+# Using TreeTagger 
+# 1) pip install treetaggerwrapper
+# 2) put treetragger in %PYHOME%\Lib\site-packages\TreeTagger
+# 3) put spanish-utf8.par and spanish-chunker.par in \TreeTagger\lib
+# See http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/spanish-tagset.txt for tag meanings
+tagger = treetaggerwrapper.TreeTagger(TAGLANG='es')
+for sentence in article_corpus.sents():
+	tagged_sentence = tagger.tag_text(sentence) 
+	tags.extend(treetaggerwrapper.make_tags(tagged_sentence))
 
-#TODO: part of speech tagging
-#TODO: POS specific stemming (dicho (V) => decir), (dicho (N) => dicho)
+#TODO: create a tagger script, save the tagged files
+#TODO: look at alternate taggers, compare
+
+#TODO: profile this and see which part is taking so long
+for tag in tags:
+	lemma = tag[2].lower()
+	if lemma not in stop_words and not non_alphabetic.search(lemma):
+		words.append(lemma)
 
 freq_dist = FreqDist(words)
 
